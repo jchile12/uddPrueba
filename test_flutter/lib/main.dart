883138a082ea5233bff517812c72cc9b01f158f2
jchile12 <joacocalderonc@gmail.com>
2025-09-02 16:24:14 -0400

    02/09/2025. Se agregó splashcreen y elementos al main

--- test_flutter/lib/main.dart
+++ test_flutter/lib/main.dart
@@ -1,4 +1,7 @@
+// main.dart
 import 'package:flutter/material.dart';
+import 'package:test_flutter/screens/splashscreen.dart'; // Importa el archivo splashscreen.dart
+import "package:test_flutter/screens/chats.dart";
 
 void main() {
   runApp(const MainApp());
@@ -10,87 +13,31 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          leading: const Icon(Icons.bathtub), // Ícono de menú
-          title: const Text('Diagramación'), // Título de la AppBar
-          actions: const [
-            Icon(Icons.baby_changing_station), // Ícono de more_vert
-          ],
-        ),
-        body: ListView(
-          padding: const EdgeInsets.all(12.0), // Padding para que no esté pegado a los bordes
-          children: [
-            Container(
-              height: 570.0,
-              width: double.infinity,
-              color: Colors.grey,
-              child: const Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text('Hello World!'),
-                    SizedBox(height: 10),
-                    Icon(Icons.baby_changing_station),
-                  ],
-                ),
-              ),
-            ),
-            const SizedBox(height: 12.0), // Espacio de 12 píxeles entre los containers
-            Container(
-              height: 148.0, // Altura de 148 píxeles
-              width: double.infinity, // Ocupa todo el ancho disponible
-              color: Colors.blue, // Color de fondo azul
-              child: const Center(child: Text('Segundo Container')), // Texto dentro del segundo Container
-            ),
-            const SizedBox(height: 12.0), // Espacio de 12 píxeles entre los containers
-            Container(
-              height: 148.0, // Altura de 148 píxeles
-              width: double.infinity, // Ocupa todo el ancho disponible
-              color: Colors.blue, // Color de fondo azul
-              child: const Center(child: Text('Tercer Container')), // Texto dentro del tercer Container
-            ),
-            const SizedBox(height: 12.0), // Espacio de 12 píxeles entre los containers
-            Container(
-              height: 148.0, // Altura de 148 píxeles
-              width: double.infinity, // Ocupa todo el ancho disponible
-              color: Colors.blue, // Color de fondo azul
-              child: const Center(child: Text('Cuarto Container')), // Texto dentro del cuarto Container
-            ),
-            const SizedBox(height: 12.0), // Espacio de 12 píxeles entre los containers
-
-            Container(
-              height: 148.0, // Altura de 148 píxeles
-              color: Colors.transparent, // Color de fondo transparente para la separación
-
-              child: Row(
-
-                children: [
-
-                  Expanded(
-                    child: Container(
-                      color: Colors.orange, // Color de fondo naranja
-                    ),
-                  ),
-
-
-                  const SizedBox(width: 12.0), // Espacio de 12 píxeles entre los containers
-                  
-                  Expanded(
-                    child: Container(
-                      color: Colors.orange, // Color de fondo naranja
-                    ),
-                  ),
-                ],
-              ),
-            ),
-
-
-
-
-          ],
-        ),
-      ),
+      themeMode: ThemeMode.system, // Cambia según la configuración del sistema
+      home: const SplashScreen(), // La primera pantalla será el SplashScreen
     );
+  }
+}
+
+// Aquí tienes la pantalla que será el home después del splash
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body:Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Soy la home de esta app"),
+            Text("Soy la home de esta app"),
+            Text("Soy la home de esta app"),
+            Text("Soy la home de esta app"),
+            Icon(Icons.bathtub),
+          ],
+        ),
+      )
+    ); // Usa el ChatsScreen en lugar de la lista
   }
 }
